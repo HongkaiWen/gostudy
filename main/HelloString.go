34a18ce3f07780ec107776833ff470d5319a1943
hongkai <446410466@qq.com>
2016-03-16 12:55:39 +0800

    init

---
+++ main/HelloString.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("abc")
+	name := "suzhou"
+	fmt.Println(name[0])
+	fmt.Println(len(name))
+	//	for(a : range name){
+	//		fmt.Println(a)
+	//	}
+}
